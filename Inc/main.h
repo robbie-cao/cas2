6024a7dab0f0ba12a23252163e51a99bf77a695a
Robbie Cao <robbie.cao@gmail.com>
2017-07-17 19:34:28 +0800

    Change uart port assignment
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- Inc/main.h
+++ Inc/main.h
@@ -59,8 +59,8 @@
 #define COMM_SEND_BUF_MAX    256
 #define REC_TIM_DELAY           50
 
-#define WIFI_COMM_UART          huart3  // -> uart6
-#define CO2_S8_UART             huart2  // -> uart3
+#define WIFI_COMM_UART          huart6  // -> uart6
+#define CO2_S8_UART             huart3  // -> uart3
 #define PM25_UART               huart2  // -> uart2
 
 #define PM25_THRESHOLD          100
